80fad440b8647b1f42671fff00d994eca6606361
Richesh Gupta <41264401+richeshgupta@users.noreply.github.com>
2020-03-04 16:46:29 +0530

    UI + faq changes
    UI + faq changes

--- js/manual.js
+++ js/manual.js
@@ -1,6 +1,6 @@
 ScrollReveal({reset:false}).reveal('.jumbotron');
 ScrollReveal({reset:false}).reveal('#countdown',{delay:700});
 ScrollReveal({reset:false}).reveal('#main-text',{delay:200});
-ScrollReveal({reset:true}).reveal('#para-text',{delay:300});
-ScrollReveal({reset:true}).reveal('#div-heading',{delay:300});
-ScrollReveal({reset:true}).reveal('#heading-hr',{delay:300});
+ScrollReveal({reset:false}).reveal('#para-text',{delay:300});
+ScrollReveal({reset:false}).reveal('#div-heading',{delay:300});
+ScrollReveal({reset:false}).reveal('#heading-hr',{delay:300});
